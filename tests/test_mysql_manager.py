9f796195256a977b884e15030538287864312494
main-voice <voicemainid73@gmail.com>
2024-03-19 22:55:49 +0800

    Refactor database engine

--- tests/test_mysql_manager.py
+++ tests/test_mysql_manager.py
@@ -1,11 +1,12 @@
 import unittest
 
-from text_to_sql.database.mysql_manager import MySQLManager
+from text_to_sql.database.db_engine import MySQLEngine
+from text_to_sql.database.db_config import DBConfig
 
 
-class TestMySQLManager(unittest.TestCase):
+class TestMySQLEngine(unittest.TestCase):
     """
-    Class to test MySQLManager
+    Class to test MySQLEngine
     """
 
     @classmethod
@@ -16,24 +17,24 @@ class TestMySQLManager(unittest.TestCase):
         cls.test_db_name = "jk_space"
 
     def test_no_db_name(self):
-        mysql_manager = MySQLManager()
-        result = mysql_manager.execute_query(self.test_selected_query)
+        mysql_engine = MySQLEngine()
+        result = mysql_engine.execute_query(self.test_selected_query)
         assert result is not None
         assert result[0][0] == self.test_selected_query_expected_result
 
     def test_with_db_name(self):
-        mysql_manager = MySQLManager(db_name=self.test_db_name)
-        result = mysql_manager.execute_query(self.test_selected_query)
+        mysql_engine = MySQLEngine(DBConfig(db_name=self.test_db_name))
+        result = mysql_engine.execute_query(self.test_selected_query)
         assert result is not None
         assert result[0][0] == self.test_selected_query_expected_result
 
     def test_not_select_query(self):
-        mysql_manager = MySQLManager()
+        mysql_engine = MySQLEngine()
         test_insert_sql = (
             "INSERT INTO jk_space.oss_auth (endpoint, access_key_id, access_key_secret) VALUES ('test', 'test', 'test')"
         )
         with self.assertRaises(ValueError):
-            mysql_manager.execute_query(test_insert_sql)
+            mysql_engine.execute_query(test_insert_sql)
 
 
 if __name__ == "__main__":
