d26fa37bb5ac1598b377f0bdb4607a7d346946c2
main-voice <voicemainid73@gmail.com>
2024-03-25 21:12:17 +0800

    add langchain test case to compare

--- tests/test_sql_generator_agent.py
+++ tests/test_sql_generator_agent.py
@@ -1,6 +1,8 @@
 import unittest
 from typing import List, Tuple
 
+from langchain_community.utilities.sql_database import SQLDatabase
+
 from text_to_sql.database.db_config import DBConfig
 from text_to_sql.database.db_engine import MySQLEngine
 from text_to_sql.database.db_metadata_manager import DBMetadataManager
@@ -47,6 +49,32 @@ class TestSQLGeneratorAgent(unittest.TestCase):
         else:
             logger.warning("The return type of SQL result is not List[Tuple], please check!")
             exit(1)
+
+    def test_langchain_agent(self):
+        from langchain.chains import create_sql_query_chain
+        from sqlalchemy import create_engine
+
+        # create a langchain database engine (SQLAlchemy)
+        lang_db = SQLDatabase(
+            engine=create_engine(self.sql_generator_agent.db_metadata_manager.db_engine.get_connection_url())
+        )
+
+        sql_chain = create_sql_query_chain(
+            llm=self.sql_generator_agent.llm_proxy.llm,
+            db=lang_db,
+        )
+
+        # msg = {"question": "show me the names of all users"}
+        msg = {"question": "找到发帖数量最多的用户"}
+        _sql = sql_chain.invoke(msg)
+
+        sql_result = self.sql_generator_agent.db_metadata_manager.db_engine.execute(_sql)
+        print(f"SQL result: {sql_result}")
+
+        if isinstance(sql_result, List) and isinstance(sql_result[0], Tuple):
+            self.assertTrue("baokker" in sql_result[0])
+        else:
+            logger.warning(f"The return type of SQL result is not List[Tuple], but {type(sql_result)} please check!")
 
 
 if __name__ == "__main__":
