27a9bd9898bf95b7ebb69e83574bcee016d4e952
main-voice <voicemainid73@gmail.com>
2024-04-19 19:51:14 +0800

    add sentry

--- text_to_sql/main.py
+++ text_to_sql/main.py
@@ -1,9 +1,19 @@
+"""Entrypoint for the text_to_sql UI."""
+
 # Example of using the SQLGeneratorAgent to generate SQL from a question
 
-from text_to_sql.config.settings import TOP_K
+from text_to_sql.config.settings import ENVIRONMENT, SENTRY_DSN, TOP_K
 from text_to_sql.ui.chatbot import SQLChatbot
 
 if __name__ == "__main__":
+    if SENTRY_DSN:
+        # lazy import
+        import sentry_sdk
+
+        sentry_sdk.init(
+            dsn=SENTRY_DSN,
+            environment=ENVIRONMENT,
+        )
     examples = [
         "Find the user who has the most posts",
         "找到发帖数量最多的用户",
